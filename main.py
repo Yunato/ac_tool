d561794551091429044e03468555caffab14cb53
Yunato <yunato525@gmail.com>
2019-07-07 14:25:48 +0900

    Add println func

--- main.py
+++ main.py
@@ -8,29 +8,36 @@ def say_hello():
     print("Hello, World!")
 
 
+def println(msg):
+    output = msg if type(msg) == str else " ".join(msg)
+    print(output + "\n")
+
+
 def print_directory():
+    print("ls")
     directories = os.listdir(current_directory)
-    print(directories)
+    println(directories)
 
 
 def get_current_directory():
     global current_directory
+    print("pwd")
     current_directory = os.getcwd()
-    print(current_directory)
+    println(current_directory)
 
 
 def make_directory(dir_name):
     global current_directory
+    print("mkdir")
     new_directory = current_directory + "/" + dir_name
     if not os.path.isdir(new_directory):
-        print(new_directory)
+        println(new_directory)
         os.mkdir(new_directory)
     else:
-        print(f"\"{dir_name}\" is already exist at {current_directory}.")
+        println(f"\"{dir_name}\" is already exist at {current_directory}.")
 
 
 if __name__ == '__main__':
-    say_hello()
     get_current_directory()
     print_directory()
     make_directory("AtCoder")
