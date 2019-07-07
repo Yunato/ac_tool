d8c15668b34bddac94edadd95de67230482e0ecc
Yunato <yunato525@gmail.com>
2019-07-07 15:33:12 +0900

    Refactoring

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import shutil
+import logging
 
 
+# logging.basicConfig(level=logging.DEBUG)
 current_directory = ""
 
 
@@ -25,54 +27,75 @@ def println(msg):
     print(output + "\n")
 
 
-def print_directory():
-    print("ls")
+def list_directory():
+    logging.debug("ls")
     directories = os.listdir(current_directory)
     println(directories)
 
 
-def get_current_directory():
+def print_working_directory():
     global current_directory
-    print("pwd")
-    current_directory = os.getcwd()
+    logging.debug("pwd")
+    # current_directory = os.getcwd()
     println(current_directory)
 
 
 def make_directory(dir_name):
     global current_directory
-    print(f"mkdir {dir_name}")
+    logging.debug(f"mkdir {dir_name}")
     new_directory = current_directory + "/" + dir_name
     if not os.path.isdir(new_directory):
-        println(new_directory)
         os.mkdir(new_directory)
+        println(new_directory)
     else:
-        println(f"\"{dir_name}\" is already exist at {current_directory}.")
+        println(f"A directory with name \"{dir_name}\" already exists at {current_directory}.")
 
 
 def change_directory(dir_name):
     global current_directory
-    print(f"cd {dir_name}")
-    current_directory += "/" + dir_name
+    logging.debug(f"cd {dir_name}")
+    next_directory = current_directory + "/" + dir_name
+    if os.path.isdir(next_directory):
+        current_directory = next_directory
+    else:
+        println(f"No such directory: {dir_name}")
 
 
 def copy_directory(source_dir, target_dir):
     global current_directory
-    print(f"cp {source_dir} {target_dir}")
-    shutil.copytree(source_dir, target_dir)
+    logging.debug(f"cp {source_dir} {target_dir}")
+    absolute_source_path = get_absolute_path(source_dir)
+    absolute_target_path = get_absolute_path(target_dir)
+    if os.path.isdir(absolute_source_path):
+        shutil.copytree(absolute_source_path, absolute_target_path)
+    else:
+        println(f"No such directory: {absolute_source_path}")
 
 
 if __name__ == '__main__':
+    current_directory = os.getcwd()
     while True:
         cmd = input().split()
         if cmd[0] == "ls":
-            print_directory()
+            list_directory()
         elif cmd[0] == "pwd":
-            get_current_directory()
+            print_working_directory()
         elif cmd[0] == "mkdir":
-            make_directory(cmd[1])
+            if len(cmd) == 2:
+                make_directory(cmd[1])
+            else:
+                println(f"usage: mkdir directory")
         elif cmd[0] == "cd":
-            change_directory(cmd[1])
+            if len(cmd) == 2:
+                change_directory(cmd[1])
+            else:
+                println(f"usage: cd directory")
         elif cmd[0] == "cp":
-            copy_directory(cmd[1], cmd[2])
+            if len(cmd) == 3:
+                copy_directory(cmd[1], cmd[2])
+            else:
+                println(f"usage: cp source_directory target_directory")
+        elif cmd[0] == "exit":
+            break
         else:
-            println("Not found")
+            println(f"Command not found: {cmd[0]}")
