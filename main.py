b164dab0dde62090ae96981b96d19166ce053e69
Yunato <yunato525@gmail.com>
2019-07-09 14:26:05 +0900

    Add validate_url() func and extract_contest_name() func

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import directory
 
 
+__require_keyword = "atcoder.jp"
+
+
 def make_and_change_directory(dir_name):
     directory.make_directory(dir_name)
     directory.change_directory(dir_name)
 
 
+def validate_url(url):
+    if __require_keyword in url:
+        return True
+    else:
+        return False
+
+
+# TODO  Correspond to below format: https://xxx.contest.atcoder.jp/
+def extract_contest_name(url):
+    separated = url.split("/")
+    return separated[4]
+
+
 if __name__ == '__main__':
     make_and_change_directory("AtCoder")
-    # TODO Input URL top page of a contest
-    # TODO Plus, Get number and name of questions
-    contest_name = input('Please input contest name you want to join. (ex. ABC001)')
+    contest_url = ""
+    # TODO Exit
+    while not validate_url(contest_url):
+        print('Please input url of contest (ex. https://atcoder.jp/contests/xxx)\n')
+        contest_url = input("'Use exit() to exit'\n")
+    contest_name = extract_contest_name(contest_url)
     print(contest_name)
+    # TODO Plus, Get number and name of questions
+
     make_and_change_directory(contest_name)
     directory.print_working_directory()
     directory.copy_directory("../../template", "./QA")
