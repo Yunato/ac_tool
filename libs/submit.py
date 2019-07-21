1c1de1d52c4bad8d4b7afafe64aa5cfbc0c5668a
Yunato <yunato525@gmail.com>
2019-07-21 22:45:36 +0900

    Add get_task_id() func at submit.py to get id of task for submission of answer

--- libs/submit.py
+++ libs/submit.py
@@ -5,8 +5,6 @@ from libs import connection
 
 def get_submit_page_url(contest_url):
     top_page = connection.get_page_text(contest_url)
-    print(top_page)
-    print()
     soup = BeautifulSoup(top_page, "html.parser")
     list_items = soup.findAll("li")
     list_item = [item for item in list_items if "Submit" in item.getText()]
@@ -14,12 +12,30 @@ def get_submit_page_url(contest_url):
     return contest_url + href[href.rfind("/"):]
 
 
+def get_task_id(html, file_info):
+    file_name = file_info[0]
+    question_name = file_name[file_name.rfind("/") + 1:file_name.rfind(".")].upper()
+    soup = BeautifulSoup(html, "html.parser")
+    selects = soup.findAll("select")
+    options = None
+    for select in selects:
+        if "task" in select["id"]:
+            options = select.findAll("option")
+            break
+    if options is None:
+        return None
+    for option in options:
+        if option.text.find(question_name) == 0:
+            return option["value"]
+
+
 def run(contest_url, file_info):
     if not contest_url:
         return False
     submit_url = get_submit_page_url(contest_url)
     submit_html = connection.get_page_text(submit_url)
-    print(submit_html)
+    task_id = get_task_id(submit_html, file_info)
+    print(task_id)
     return True
 
 
